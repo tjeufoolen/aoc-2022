90e70c96abf11bba8f4304ab1c490e5d9d13ccc7
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2022-12-06 14:28:59 +0100

    Setup day7

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ https://adventofcode.com/2022
 4. [Camp Cleanup](src/main/java/dev/foolen/aoc2022/solutions/Day04.java)
 5. [Supply Stacks](src/main/java/dev/foolen/aoc2022/solutions/Day05.java)
 6. [Tuning Trouble](src/main/java/dev/foolen/aoc2022/solutions/Day06.java)
+7. [???](src/main/java/dev/foolen/aoc2022/solutions/Day07.java)
 
 ## Author
 
---
+++ src/main/java/dev/foolen/aoc2022/solutions/Day07.java
@@ -0,0 +1,27 @@
+package dev.foolen.aoc2022.solutions;
+
+import dev.foolen.aoc2022.commons.utils.IOUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day07 {
+    private static List<String> input;
+
+    public static void main(String[] args) throws IOException {
+        input = IOUtil.readFileAsListOfStrings( "input/day07.txt");
+
+        solvePartOne();
+        solvePartTwo();
+    }
+
+    private static void solvePartOne() {
+        System.out.printf("[PartOne] first marker after character: %s%n", 0);
+        // - ?
+    }
+
+    private static void solvePartTwo() {
+        System.out.printf("[PartTwo] first marker after character: %s%n", 0);
+        // - ?
+    }
+}
