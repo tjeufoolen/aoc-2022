f2fb695713f532b5940b962815c2d8731a08fce3
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2022-12-06 14:18:08 +0100

    Completed day 6

--- src/main/java/dev/foolen/aoc2022/solutions/Day06.java
+++ src/main/java/dev/foolen/aoc2022/solutions/Day06.java
@@ -3,6 +3,7 @@ package dev.foolen.aoc2022.solutions;
 import dev.foolen.aoc2022.commons.utils.IOUtil;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day06 {
@@ -16,12 +17,23 @@ public class Day06 {
     }
 
     private static void solvePartOne() {
-        System.out.printf("[PartOne] result: %s%n", 0);
-        // - ?
+        int marker = getFirstMarker(input.get(0), 4);
+        System.out.printf("[PartOne] first marker after character: %s%n", marker);
+        // - 1702
     }
 
     private static void solvePartTwo() {
-        System.out.printf("[PartTwo] result: %s%n", 0);
-        // - ?
+        int marker = getFirstMarker(input.get(0), 14);
+        System.out.printf("[PartTwo] first marker after character: %s%n", marker);
+        // - 3559
+    }
+
+    private static int getFirstMarker(String buffer, int nbOfDistinctCharacters) {
+        List<String> parts = Arrays.stream(buffer.split("")).toList();
+        for (int i=nbOfDistinctCharacters-1; i< parts.size(); i++) {
+            List<String> lastFour = parts.subList(i-(nbOfDistinctCharacters-1), i+1);
+            if (lastFour.stream().distinct().count() == nbOfDistinctCharacters) return i + 1;
+        }
+        return -1;
     }
 }
