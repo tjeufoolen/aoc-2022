1a69295e409efe7d65bfcddd6b5f01d91b8ec167
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2022-12-06 11:29:35 +0100

    Setup day5

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ https://adventofcode.com/2022
 2. [Rock Paper Scissors](src/main/java/dev/foolen/aoc2022/solutions/Day02.java)
 3. [Rucksack Reorganization](src/main/java/dev/foolen/aoc2022/solutions/Day03.java)
 4. [Camp Cleanup](src/main/java/dev/foolen/aoc2022/solutions/Day04.java)
+5. [Supply Stacks](src/main/java/dev/foolen/aoc2022/solutions/Day05.java)
 
 ## Author
 
---
+++ src/main/java/dev/foolen/aoc2022/solutions/Day05.java
@@ -0,0 +1,30 @@
+package dev.foolen.aoc2022.solutions;
+
+import dev.foolen.aoc2022.commons.utils.IOUtil;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day05 {
+    private static List<String> input;
+
+    public static void main(String[] args) throws IOException {
+        input = IOUtil.readFileAsListOfStrings( "input/day05.txt");
+
+        solvePartOne();
+        solvePartTwo();
+    }
+
+    private static void solvePartOne() {
+        System.out.printf("[PartOne] result: %d%n", 0);
+        // - ?
+    }
+
+    private static void solvePartTwo() {
+        System.out.printf("[PartTwo] result: %d%n", 0);
+        // - ?
+    }
+}
